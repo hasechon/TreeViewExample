ea4324c2364add14f98b65335ad5d35940f0d940
hasechon <drivevolleyer@gmail.com>
2024-07-19 00:44:44 +0900

    TreeItemViewModelを追加

---
+++ TreeViewExample/TreeItemViewModel.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "TreeItemViewModel.h"
+#if __has_include("TreeItemViewModel.g.cpp")
+#include "TreeItemViewModel.g.cpp"
+#endif
+
+namespace winrt::TreeViewExample::implementation
+{
+	TreeItemViewModel::TreeItemViewModel() : name_(L"default")
+	{
+	}
+
+	TreeItemViewModel::TreeItemViewModel(hstring name) : name_(name)
+	{
+	}
+
+	hstring TreeItemViewModel::Name()
+	{
+		return name_;
+	}
+
+	Windows::Foundation::Collections::IObservableVector<TreeViewExample::TreeItemViewModel> TreeItemViewModel::Children()
+	{
+		return children_;
+	}
+}
---
+++ TreeViewExample/TreeItemViewModel.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "TreeItemViewModel.g.h"
+
+namespace winrt::TreeViewExample::implementation
+{
+	struct TreeItemViewModel : TreeItemViewModelT<TreeItemViewModel>
+	{
+	public:
+		TreeItemViewModel();
+		TreeItemViewModel(hstring);
+
+		hstring Name();
+		Windows::Foundation::Collections::IObservableVector<TreeViewExample::TreeItemViewModel> Children();
+
+	private:
+		hstring name_;
+		Windows::Foundation::Collections::IObservableVector<TreeViewExample::TreeItemViewModel> children_{nullptr};
+	};
+}
+
+namespace winrt::TreeViewExample::factory_implementation
+{
+	struct TreeItemViewModel : TreeItemViewModelT<TreeItemViewModel, implementation::TreeItemViewModel>
+	{
+	};
+}
